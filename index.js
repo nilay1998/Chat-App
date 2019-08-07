028a0b878a9110b1ca3f889c2b82f37830220ff5
Nilay Gupta <nilaygupta1998@gmail.com>
2019-08-07 20:57:19 +0530

    mongodb

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const mongoose=require('mongoose');
 
 require('./prod')(app);
 
-mongoose.connect('mongodb+srv://admin:admin1234@cluster0-vizhy.mongodb.net/test?retryWrites=true&w=majority')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://admin:admin1234@cluster0-vizhy.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));

bc90bf8e7109fe1e6317229eb133facc50f783d6
Nilay Gupta <nilaygupta1998@gmail.com>
2020-04-16 02:07:49 +0530

    dir for backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,54 @@
-const express = require('express');
-const app = express();
-const bodyParser=require('body-parser');
-const student=require('./routes/student');
-const teacher=require('./routes/teacher');
-const mongoose=require('mongoose');
-
+const express=require('express');
+const socket=require('socket.io');
+const mongoose = require('mongoose');
+const Chat  = require("./message");
+const app=express();
 require('./prod')(app);
 
-mongoose.connect('mongodb+srv://admin:admin1234@cluster0-vizhy.mongodb.net/test?retryWrites=true&w=majority')
+
+const config=require('config');
+
+if(!config.get('db'))
+{
+  console.error('FATAL ERROR: Database is not defined.');
+  process.exit(1);
+}
+
+
+mongoose.connect(config.get('db'))
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/api',student);
-app.use('/api',teacher);
-
-app.get('/api/get',async(req,res)=>{
-    res.json({message:'RUNNING'});
-});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const server=app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const io=socket(server);
+
+app.get('/', async(req,res)=>{
+    const chat = await Chat.find();
+    res.send(chat);
+});
+
+io.on('connection', function(socket){
+    console.log('MADE SOCKET CONNECTION');
+
+    socket.on('join', userNickname => {
+        console.log(userNickname +" : has joined the chat");
+        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat");
+    });
+
+    socket.on('messagedetection', async (senderNickname, messageContent) => {
+        console.log(senderNickname+" : " +messageContent)
+        const message = { 'message':messageContent,'senderNickname':senderNickname};
+        io.emit('message',message);
+
+        let  chatMessage  =  new Chat({ message: messageContent, sender: senderNickname});
+        await chatMessage.save();
+    });
+
+    socket.on('kill', abc => {
+        console.log(abc);
+        socket.broadcast.emit( "userdisconnect" ,abc + " : user has left");
+    });
+});
